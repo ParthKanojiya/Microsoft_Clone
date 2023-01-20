d23d10d7510c8f228e4c7a3a1702650d4fe6448f
Parth Kanojiya <101059947+ParthKanojiya@users.noreply.github.com>
2023-01-20 15:25:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    menu = document.getElementById("navigation-menu");
+
+    if(menu.style.display == "none") {
+        menu.style.display = "block"
+    }
+    else {
+        menu.style.display = "none";
+    }
+}
