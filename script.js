66bcba789ecbc30c300b73bfeb3825ff87d79294
Parth Kanojiya <101059947+ParthKanojiya@users.noreply.github.com>
2023-01-20 16:42:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function myFunction() {
     menu = document.getElementById("navigation-menu");
 
-    if(menu.style.display == "none") {
+    if (menu.style.display == "none") {
         menu.style.display = "block"
+        menu.style.transform = "translateX(0)";
     }
     else {
         menu.style.display = "none";
+        menu.style.transform = "translateX(-100%)";
     }
 }
